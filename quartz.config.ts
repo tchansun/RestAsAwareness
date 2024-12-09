aef8a97fb83ac888de6cd7e98c053033518de600
Frederic <suntchan@gmail.com>
2024-12-09 20:32:11 +0100

    Quartz sync: Dec 9, 2024, 8:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "./IMAGES/RAALogo.png",
+    pageTitle: string,
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
