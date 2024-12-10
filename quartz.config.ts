7f2b1fa05cfec8eec9be10313f4247372d978a41
Frederic <suntchan@gmail.com>
2024-12-10 07:59:17 +0100

    Quartz sync: Dec 10, 2024, 7:59 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: string,
+    pageTitle: "Rest As Awareness",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
